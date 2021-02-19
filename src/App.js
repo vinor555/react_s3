6f400f86bc1e1518ff1489526a7b8a4b4ad67c74
kacruzrh <soporte@redchapina.com>
2021-02-19 17:19:45 -0600

    agregando camara y nombre

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+const WebcamComponent = () => <Webcam />;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <WebcamComponent />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kevin Ariel Cruz Ortiz - 201213059
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
